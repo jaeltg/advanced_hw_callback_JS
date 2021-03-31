db5affa6ea4660e02e1e0816d216c50b9cc03e2a
jaeltg <jaeltg@hotmail.com>
2021-03-31 19:47:07 +0100

    attempted isogram

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,10 +1,14 @@
 const IsogramFinder = function (word) {
     this.word = word
-
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-    letters = this.word.split('')
+    let lowerCase = this.word.toLowerCase()
+    let letters = lowerCase.split('')
+    let noDuplicates = new Set(letters)
+    return letters.every((letter) => {
+        return noDuplicates.has(letter)
+    })
 
 }
 
